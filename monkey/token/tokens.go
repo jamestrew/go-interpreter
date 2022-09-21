96b549b9ef3d822d89c56bcc9138208198779b60
James Trew <j.trew10@gmail.com>
2022-09-20 21:23:54 -0400

    feat: init some tokens

---
+++ monkey/token/tokens.go
@@ -0,0 +1,23 @@
+package token
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	IDENT = "IDENT"
+	INT = "INT"
+
+	ASSIGN = "="
+	PLUS = "+"
+
+	COMMA = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)
