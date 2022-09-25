dcf178f3b44a386ed3f9b035f24c1c83abde59bc
James Trew <j.trew10@gmail.com>
2022-09-25 18:46:57 -0400

    feat: init parser (parser let statement)

---
+++ monkey/parser/parser.go
@@ -0,0 +1,102 @@
+package parser
+
+import (
+	"github.com/jamestrew/go-interpreter/monkey/ast"
+	"github.com/jamestrew/go-interpreter/monkey/lexer"
+	"github.com/jamestrew/go-interpreter/monkey/token"
+)
+
+
+type Parser struct {
+	lexer *lexer.Lexer
+	curToken token.Token
+	peekToken token.Token
+}
+
+func New(lexer *lexer.Lexer) *Parser {
+	p := &Parser{lexer: lexer}
+	p.setInitialTokens()
+	return p
+}
+
+func (p *Parser) setInitialTokens() {
+	p.nextToken()
+	p.nextToken()
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.lexer.NextToken()
+}
+
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	}
+	return false
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal }
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+
+	// TODO: assign value
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+// func (p *Parser) parseReturnStatement() {}
+// func (p *Parser) parseIfStatement() {}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	// case token.RETURN:
+	// 	return p.parseReturnStatement()
+	// case token.IF:
+	// 	return p.parseIfStatement()
+
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	statements := []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			statements = append(statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return &ast.Program{Statements: statements}
+}
+
+
