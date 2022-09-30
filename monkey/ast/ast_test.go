d086d11471e412c91ccb142778408d470ee844cd
James Trew <j.trew10@gmail.com>
2022-09-30 17:13:09 -0400

    feat: create ast string representation

---
+++ monkey/ast/ast_test.go
@@ -0,0 +1,31 @@
+package ast
+
+import (
+	"testing"
+
+	"github.com/jamestrew/go-interpreter/monkey/token"
+)
+
+func TestString(t *testing.T) {
+	input := "let myVar = anotherVar;"
+
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+
+	if program.String() != input {
+		t.Errorf("program.String() expect %s. got=%q", input, program.String())
+	}
+}
