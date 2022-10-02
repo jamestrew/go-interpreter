d4bcb285b683070e7164b1ff3c19762508c9e126
James Trew <j.trew10@gmail.com>
2022-10-02 15:30:21 -0400

    refactor: organize AST structs/interfaces

--- monkey/ast/ast.go
+++ monkey/ast/ast.go
@@ -6,64 +6,6 @@ import (
 	"github.com/jamestrew/go-interpreter/monkey/token"
 )
 
-type Node interface {
-	TokenLiteral() string
-	String() string
-}
-
-type Statement interface {
-	Node
-	statementNode()
-}
-
-type Expression interface {
-	Node
-	expressionNode()
-}
-
-type Program struct {
-	Statements []Statement
-}
-
-type Identifier struct {
-	Token token.Token
-	Value string
-}
-
-type IntegerLiteral struct {
-	Token token.Token
-	Value int64
-}
-
-type PrefixExpression struct {
-	Token    token.Token
-	Operator string
-	Right    Expression
-}
-
-type InfixExpression struct {
-	Token    token.Token
-	Left     Expression
-	Operator string
-	Right    Expression
-}
-
-type LetStatement struct {
-	Token token.Token
-	Name  *Identifier
-	Value Expression
-}
-
-type ReturnStatement struct {
-	Token token.Token
-	Value Expression
-}
-
-type ExpressionStatement struct {
-	Token      token.Token
-	Expression Expression
-}
-
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
