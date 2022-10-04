e5b55c045366ccee39924cd856ef63913700a00b
James Trew <j.trew10@gmail.com>
2022-10-04 19:59:14 -0400

    feat: parse grouped expressions

--- monkey/parser/parser.go
+++ monkey/parser/parser.go
@@ -52,6 +52,7 @@ func New(lexer *lexer.Lexer) *Parser {
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 	p.registerPrefix(token.TRUE, p.parseBoolean)
 	p.registerPrefix(token.FALSE, p.parseBoolean)
+	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
 
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfix(token.EQ, p.parseInfixExpression)
@@ -151,6 +152,15 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
 
 func (p *Parser) parseBoolean() ast.Expression {
 	exp := &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
+	return exp
+}
+
+func (p *Parser) parseGroupedExpression() ast.Expression {
+	p.nextToken()
+	exp := p.parseExpression(LOWEST)
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
 	return exp
 }
 
