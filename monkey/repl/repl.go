dc61c336d1970c4333c427629442a18755d3a973
James Trew <j.trew10@gmail.com>
2022-10-07 17:41:13 -0400

    feat: add parsing to repl thing

--- monkey/repl/repl.go
+++ monkey/repl/repl.go
@@ -3,12 +3,21 @@ package repl
 import (
 	"bufio"
 	"fmt"
-	"github.com/jamestrew/go-interpreter/monkey/lexer"
-	"github.com/jamestrew/go-interpreter/monkey/token"
 	"io"
+
+	"github.com/jamestrew/go-interpreter/monkey/lexer"
+	"github.com/jamestrew/go-interpreter/monkey/parser"
 )
 
 const PROMPT = ">> "
+
+func printParseErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t")
+		io.WriteString(out, msg)
+		io.WriteString(out, "\n")
+	}
+}
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -21,8 +30,15 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		lexer := lexer.New(line)
-		for tok := lexer.NextToken(); tok.Type != token.EOF; tok = lexer.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(lexer)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
 	}
 }
